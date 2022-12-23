591ef0468e85c5334638717a5e99a5b8dcdb350a
Rawipa <110955515+nnrwp@users.noreply.github.com>
2022-12-23 16:01:49 +0700

    complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,41 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+  int age, h, b;
+  string name;
+  cout << "Enter your age: ";
+  cin >> age;
+  if(age <= 25){
+    cout << "Enter your height: ";
+    cin >> h;
+    if(h < 100){
+      name = "Chopper";
+    }
+    else if(h < 180){
+      name = "Usopp";
+    }
+    else{
+      cout << "Enter your bounty: ";
+      cin >> b;
+      if(b > 1.1e9){
+        name = "Zoro";
+      }else{
+        name = "Sanji";
+      }
+    }
+  }
+  else if(age <= 60){
+    cout << "Enter your bounty: ";
+    cin >> b;
+    if(b > 500e6){
+      name = "Jinbe";
+    }else{
+      name = "Franky";
+    }
+  }else{
+    name = "Brook";
+  }
+  cout << "Your character = " << name;
+}
